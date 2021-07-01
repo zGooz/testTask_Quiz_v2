70fc6495deaf99b9e14f7c1dfa0d0bb64f2395c3
zGooz <ya.asde-12a@yandex.ru>
2021-07-01 15:22:34 +0300

    added sprites for numbers and letters. Filled in with these providers

---
+++ Assets/Scripts/Provider/LevelsProperty.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Provider))]
+
+public class LevelsProperty : MonoBehaviour
+{
+    private IReadOnlyCollection<Content> Batch => _batch;
+
+    [SerializeField]
+    private List<int> _levelSizes;
+
+    private Provider _provider;
+    private IReadOnlyCollection<Content> _batch;
+
+    private void Awake()
+    {
+        _provider = GetComponent<Provider>();
+        _batch = _provider.GetRandomBatch();
+    }
+}
---
+++ Assets/Scripts/Provider/Provider.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class Provider : MonoBehaviour
+{
+    private List < List<Content> > _batchs = new List < List<Content> >();
+
+    // FOR EXAMPLE :
+    //      [SerializeField, Header("Animals"), Space(10)]
+    //      private List<Content> _animals;
+
+    [SerializeField, Header("Number"), Space(10)]
+    private List<Content> _numbers;
+
+    [SerializeField, Header("Letters"), Space(10)]
+    private List<Content> _letters;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    public IReadOnlyCollection<Content> GetRandomBatch()
+    {
+        var index = UnityEngine.Random.Range(0, _batchs.Count);
+        var batch = _batchs[index];
+
+        return batch;
+    }
+
+    private void Init()
+    {
+        _batchs.Add(_numbers);
+        _batchs.Add(_letters);
+    }
+}
+
+[Serializable]
+public class Content
+{
+    public Sprite icon;
+    public string sought;
+
+    public int Value { get; private set; }
+
+    public void Restore()
+    {
+        icon = null;
+        sought = "";
+        Value = 0;
+    }
+}
