f169eb7744823ffa8642657ab88f31589c0968c5
zGooz <ya.asde-12a@yandex.ru>
2021-07-01 21:09:24 +0300

    added a 'fade in' effect to the text

---
+++ Assets/Scripts/UI/PromptOfTask.cs
@@ -0,0 +1,23 @@
+﻿
+using TMPro;
+using DG.Tweening;
+using UnityEngine;
+
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+
+public class PromptOfTask : MonoBehaviour
+{
+    [SerializeField]
+    private float _duration;
+
+    private TextMeshProUGUI _field;
+
+    private void Awake()
+    {
+        _field = GetComponent<TextMeshProUGUI>();
+
+        var sequence = DOTween.Sequence();
+        sequence.Append(_field.DOFade(1f, _duration));
+    }
+}
